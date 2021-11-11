1d6a844a7d9b8f0b555371d09919ba368ec1ca48
Xaviior <stiand.mail@gmail.com>
2021-11-11 22:37:51 +0100

    yeye

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ mongoose
   });
 // SET:::::
 app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // USE:::::
 app.use(methodOverride("_method"));
@@ -27,12 +28,19 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.render("newUser");
 });
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+app.get("/vaaler", (req, res) => {
+  res.render("vaaler");
+});
 
 // Create New User
 app.post("/newUser", async (req, res) => {
   const newUser = new User(req.body);
   await newUser.save();
   console.log(req.body);
+  res.redirect("/home");
 });
 
 app.listen(expressPort, () => {
